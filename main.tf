f709792ee2f0bd912374c43ab66673febd5557f5
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-09-16 09:14:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -357,10 +357,10 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "reyaz-devopsproject-bkt-123"
+  bucket = "abbasi-devopsproject-bkt-123"
 
   tags = {
-    Name        = "reyaz-devopsproject-bkt-123"
+    Name        = "abbasi-devopsproject-bkt-123"
     Environment = "Dev"
   }
 }
